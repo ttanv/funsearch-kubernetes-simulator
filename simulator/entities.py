48e3e93e6b87612796821e559858131180523ebb
ttanv <temoor556@gmail.com>
2025-08-02 10:35:15 +0300

    Change IDs from int to str

--- simulator/entities.py
+++ simulator/entities.py
@@ -4,24 +4,30 @@ from dataclasses import dataclass
 @dataclass
 class GPU:
     """Represents single GPU inside a node"""
-    gpu_id: int
     memory_mib_left: int
     memory_mib_total: int
     
 @dataclass
 class Node:
     """Represents a node that may contain multiple GPUs"""
-    node_id: int
+    node_id: str
     cpu_milli_left: int
     cpu_milli_total: int
     memory_mib_left: int
     memory_mib_total: int
+    gpu_count: int
     gpus: list[GPU]
     
 @dataclass
+class Cluster:
+    """Represents the full cluster"""
+    nodes_dict: dict[str, Node]    
+
+"""The items of the problem"""    
+@dataclass
 class Pod:
     """Represents a pod request"""
-    pod_id: int
+    pod_id: str
     cpu_milli: int
     memory_mib: int
     num_gpu: int
