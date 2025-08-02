0210a95ad0a1bab44989750fab821bd9d94c3acc
ttanv <temoor556@gmail.com>
2025-08-02 14:21:14 +0300

    Add discrete event simulator

---
+++ simulator/event_simulator.py
@@ -0,0 +1,42 @@
+import heapq
+from dataclasses import dataclass
+from enum import StrEnum, auto
+
+from simulator.entities import Pod
+
+class EventType(StrEnum):
+    CREATION = auto()
+    DELETION = auto()
+
+@dataclass
+class Event:
+    event_type: EventType
+    pod: Pod
+
+class DiscreteEventSimulator:
+    """A discrete event simulator implementation, sorts events based on time.
+       Gets the next event in order, and allows to add new events
+    """
+    def __init__(self, pod_list: list[Pod]):
+        self.event_heap = self._populate_heap(pod_list)
+        heapq.heapify(self.event_heap)
+        return
+
+    def _populate_heap(self, pod_list: list[Pod]):
+        """Populates the heap with creation times. Deletion time depends on scheduler"""
+        events = []
+        for pod in pod_list:
+            event = Event(EventType.CREATION, pod)
+            events.append((pod.creation_time, event))
+        return events
+
+    def pop_event(self):
+        return heapq.heappop(self.event_heap)
+
+    def peek_event(self):
+        return self.event_heap[0]
+
+    def push_deletion_event(self, pod):
+        del_event = Event(EventType.DELETION, pod)
+        # Assumes pod.deletion_time has been set by a scheduler.
+        heapq.heappush(self.event_heap, (pod.deletion_time, del_event))
